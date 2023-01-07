b6c42191e0d942c087c455b9dd6c9e5ffb2a632c
Ksenia Tst <kseniatsytrikova@gmail.com>
2023-01-07 17:38:35 +0300

    Updates

---
+++ precache-manifest.a3978aef93efe2d6a2c96c4c7f7ee0f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae1b9bacfe35ec0addbafc41c0342a8",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "fafbe90cc16869446f1e",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "ccf38660fc21854aa91c",
+    "url": "/Todolist/static/js/2.ee8811eb.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.ee8811eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fafbe90cc16869446f1e",
+    "url": "/Todolist/static/js/main.6b6e826d.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
